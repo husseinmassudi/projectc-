cc7612b9c89dc835e2d2f2d37d5e7e3ba30d8b99
hussein massudi <husseinmassudi93@gmail.com>
2022-02-21 17:39:15 +0300

    add commit

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp13
 {
     internal class Program
     {
-        static string token = "enter your token";
+        static string token = "5063037329:AAEiWgvlSqMeUbf_mrA_fmT5OZeMn_8Cziw";
         static TelegramBotClient bot = new TelegramBotClient(token);
         static void Main(string[] args)
         {
@@ -14,9 +14,9 @@ namespace ConsoleApp13
             while (true)
             {
                  Telegram.Bot.Types.Update[] updates = bot.GetUpdatesAsync(x).Result;
-                
-                
-                    foreach (var update in updates)
+               
+
+                foreach (var update in updates)
                     {
                         x = update.Id + 1;
                          
@@ -24,15 +24,21 @@ namespace ConsoleApp13
                         if (update.Message != null)
                         {
                             long id = update.Message.From.Id;
-                            string txt = update.Message.Text;
+                            string txt = update.Message.Text.ToLowerInvariant();
+                           
                             string un = update.Message.From.Username;
                             string f = update.Message.From.FirstName;
                             string l = update.Message.From.LastName;
-                           
+                            DateTime time= DateTime.UtcNow;
+                            DateTime b = DateTime.Now;
+                            
 
-                            if (txt.Contains("start")|| txt.Contains("Start")|| txt.Contains("START"))
+
+
+                            if (txt.Contains("start"))
                             {
                                 bot.SendTextMessageAsync(id,"Hi, welcome  "+un+" \n If need help please\n write or click " + " /help");
+                                 
                                 Console.WriteLine(id);
                                 Console.WriteLine(txt);
                                 Console.WriteLine(un);
@@ -44,32 +50,81 @@ namespace ConsoleApp13
 
 
                             }
-                            else if (txt.Contains("help")|| txt.Contains("Help")|| txt.Contains("HELP"))
+                            else if (txt.Contains("help"))
                             {
                                 bot.SendTextMessageAsync(id,un+ " you can take help from\nProgramer " + " /hussein\n " + 
-                                    " OR Engineer" + " /mohamed ");
-                                Console.WriteLine(txt);
+                                    " OR Engineer" + " /mohamed \n"+ "to konw time now  /time ");
+                                 
+                            Console.WriteLine(txt);
 
                         }
 
-                            else if (txt.Contains("hussein") || txt.Contains("Hussein")|| txt.Contains("HUSSEIN"))
+                            else if (txt.Contains("hussein"))
                             {
-                                bot.SendTextMessageAsync(id, un+ " welcome to Programer hussein  \n you can to contact me in this aways " +
-                                    "\nwww.facebook.com\nwww.twitter.com\nwww.gmail.com\n " + " Or take help from Engineer" + " /mohamed");
-                                Console.WriteLine(txt);
-
-                        }
-                            else if (txt.Contains("mohamed")|| txt.Contains("Mohamed")|| txt.Contains("MOHAMED"))
+                            if (time.Hour > 7 && time.Hour < 19  )
                             {
-                                bot.SendTextMessageAsync(id,un+ "  welcome to Engineer mohamed  \n you can to contact me in this aways" +
-                                    "\nwww.facebook.com\nwww.twitter.com\nwww.gmail.com\n " + " Or take help from Programer " + " /hussein");
-                               Console.WriteLine(txt);
+
+
+                                bot.SendTextMessageAsync(id, un+" now clossed ("+b+" )");
+                                 bot.SendTextMessageAsync(id, "you can to contact me in 7 AM TO 7 PM");
+                                  bot.SendTextMessageAsync(id," you can take help in this time from Engineer /mohamed");
+                            }
+                            else
+                            {
+                                string facebook = "www.facebook.com";
+                                string twitter = "www.twitter.com";
+                                string gmail = "www.gmail.com";
+                                bot.SendTextMessageAsync(id, un + " welcome to Programer hussein  \n you can to contact me in this aways " +
+                                        "\n" + facebook + "\n" + twitter + "\n" + gmail);
+                                
+                                Console.WriteLine(txt);
+                            }
 
                         }
+                            else if (txt.Contains("mohamed"))
+                            {
+                            if (time.Hour > 19 && time.Hour < 7)
+                            {
+                                bot.SendTextMessageAsync(id, un + " now clossed 9" +b+" )");
+                                bot.SendTextMessageAsync(id, "you can to contact me in 7 PM TO 7 AM");
+                                bot.SendTextMessageAsync(id,"you can take help in this time from Programer /hussein");
+
+                            }
+                            else
+                            {
+                                string facebook = "www.facebook.com";
+                                string twitter = "www.twitter.com";
+                                string gmail = "www.gmail.com";
+                                bot.SendTextMessageAsync(id, un + " welcome to Programer hussein  \n you can to contact me in this aways " +
+                                        "\n" + facebook + "\n" + twitter + "\n" + gmail);
+                                
+                                Console.WriteLine(txt);
+                            }
+
+                             }
+                             else if (txt.Contains("time"))
+                             {
+                            float a;
+                            if (b.Hour > time.Hour)
+                            {
+                                bot.SendTextMessageAsync(id, b + " local time " +"+"+ (b.Hour - time.Hour) + " UTC");
+
+                            }
+                            else
+                            {
+                                bot.SendTextMessageAsync(id, b + " local time " + (b.Hour - time.Hour) + " UTC");
+                            }
+                            
+
+
+                             }
+                            
                             else
                             {
                                 bot.SendTextMessageAsync(id,un+ " I can not understand you \nthank you for visiting");
-                                 Console.WriteLine(txt);
+                                 
+                           
+
 
                             }
 
